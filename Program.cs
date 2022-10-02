48ca018a431929c5d5951c446ea7b25811cd9531
Azizbek Ochilov <99602759+azizbek-ochilov@users.noreply.github.com>
2022-10-02 22:20:10 +0500

    Teach Comparison

--- Program.cs
+++ Program.cs
@@ -10,5 +10,14 @@ System.Console.WriteLine("Converting...");
 int age = Convert.ToInt32(ageAsString);
 System.Console.WriteLine($"Successfully converted {age}");
 int keschasAge = 3;
-int ageDifference= age- keschasAge;
+int ageDifference = age - keschasAge;
+
 System.Console.WriteLine($"The difference your and Kescha's age is {ageDifference}");
+
+System.Console.WriteLine($"You are older than Kescha: {age>keschasAge}");
+System.Console.WriteLine($"You are older than or equal to Kescha: {age>=keschasAge}");
+System.Console.WriteLine($"You are younger than Kescha: {age<keschasAge}");
+System.Console.WriteLine($"You are younger than or equal to Kescha: {age<=keschasAge}");
+System.Console.WriteLine($"Your and Kescha's are equal : {age==keschasAge}");
+System.Console.WriteLine($"Your and Kescha's are not equal : {age==keschasAge}");
+
